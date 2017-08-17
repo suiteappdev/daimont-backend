96e394cbd56a13c2ca6dde902f3b86f9a40c27f9
Armitage <listerine1989@gmail.com>
2017-08-16 19:49:01 -0500

    latest

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	dburl : "mongodb://daiAdmin:0126*dai*@localhost:27017/daimont",
+	dburl : "mongodb://daiAdmin:0126*dai*@13.59.230.244:27017/daimont",
 	secret : "$daimont.2017",
 	appPort : 8080,
 	ssl_phrase : process.env.SSL_PHRASE,
