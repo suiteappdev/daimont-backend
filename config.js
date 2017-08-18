e207cecbfe2465fe98b240629fd741bc6476f6aa
Armitage <listerine1989@gmail.com>
2017-08-18 14:18:59 -0500

    latest

--- config.js
+++ config.js
@@ -5,7 +5,8 @@ module.exports = {
 	ssl_phrase : process.env.SSL_PHRASE,
 	bucket_name : 'daimontstorage',
 	email_recipient : 'listerine1989@gmail.com',
-	base_url : 'http://www.daimont.com/frontend/dist/#/',
+	base_url_dev : 'http://localhost:8080/#/',
+	base_url_pro : 'http://www.daimont.com/frontend/dist/#/',
 	smtp_email : "listerine1989@gmail.com",
 	smtp_password : 'house1989*',
 	smtp_server : 'smtp.gmail.com' 
