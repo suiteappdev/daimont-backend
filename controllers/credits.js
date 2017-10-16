c18f917de412c044244122e6e523c1033ede1851
Armitage <listerine1989@gmail.com>
2017-10-16 09:34:19 -0500

    latest

--- controllers/credits.js
+++ controllers/credits.js
@@ -382,6 +382,12 @@ module.exports = function(app, apiRoutes, io){
 
 			Model.update({ _id : mongoose.Types.ObjectId(req.params.id) } , data , function(err, rs){
 				if(rs){
+				          User.findByIdAndUpdate(mongoose.Types.ObjectId(req.params.id), {$set: {'data.banned': true, "data.banTime" : new Date(Date.now()}}, function(err, rs) {
+				              if(!err){
+				                  res.status(200).json(rs);                
+				              }
+				          });
+
 						Model.findOne({ _id : mongoose.Types.ObjectId(req.params.id) }).populate("_user").exec(function(error, credit){
 							if(!error){
 		 						var _html_credit_rejected = _compiler.render({ _data : {
