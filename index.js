acc912b6ac7be97ccb53b10e42b16ed8decc1c17
Armitage <listerine1989@gmail.com>
2017-10-18 19:33:13 -0500

    latest

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ var csrf = require('csurf');
 var cookieParser= require('cookie-parser'); 
 
 app.use(cookieParser('daimont//***2017plasmaguns'));
-app.use(csrf({ cookie: { key: 'XSRF-TOKEN', path:'/'}}));
+app.use(csrf());
 
 app.get('/form', function(req, res) {
     // pass the csrfToken to the view
