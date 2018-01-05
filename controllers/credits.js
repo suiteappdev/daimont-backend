a35dfd05650aaa866e604256a9f7ec7a05832ef2
Armitage <listerine1989@gmail.com>
2018-01-05 12:43:04 -0500

    latest

--- controllers/credits.js
+++ controllers/credits.js
@@ -523,7 +523,9 @@ module.exports = function(app, apiRoutes, io){
 			 Model.find().populate("_user").populate("_payment").exec(function(err, rs){
 					if(!err){
 						var result = rs.filter(function(credit){
-							return credit._user.data.updated;
+							if(credit._user.data){
+								return credit._user.data.updated;
+							}
 						});
 
 						res.status(200).json(result);
