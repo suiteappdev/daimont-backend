3c8e311d675309ae3e690ef8d447e1b1f5e59ff4
Armitage <listerine1989@gmail.com>
2018-02-27 10:49:15 -0500

    latest

--- controllers/certificate.js
+++ controllers/certificate.js
@@ -2,11 +2,7 @@ var User = require('../models/user');
 var mongoose = require('mongoose');
 
 module.exports = exports = function(app, apiRoutes, io){
-
 	app.get("/certificate/:id", function(req, res){
-		res.setHeader('Content-Type', 'application/pdf');
-		res.setHeader('Content-Disposition', 'attachment; filename=certificado.pdf');
-		
 		User.findOne({ _id : mongoose.Types.ObjectId(req.params.id)}).exec(function(err, data){
 			if(!err){
  				var _html = _compiler.render({ _data : { name : "andrew alexander castro vital", cc : "1103102286"} }, 'certificate/certificate.ejs');
@@ -14,7 +10,7 @@ module.exports = exports = function(app, apiRoutes, io){
 				var wkhtmltopdf = require('wkhtmltopdf');
 				wkhtmltopdf.command = "/home/ec2-user/wkhtmltox/bin/wkhtmltopdf";
 
-				stream = wkhtmltopdf(_html, { pageSize: 'letter' }).pipe(res);				
+				wkhtmltopdf(_html, { pageSize: 'letter' }).pipe(res);				
 			}
 		});
 	});
