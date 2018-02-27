7463354e06c9ae860977a6727d01861afafe4a22
Armitage <listerine1989@gmail.com>
2018-02-27 10:51:29 -0500

    latest

--- controllers/certificate.js
+++ controllers/certificate.js
@@ -5,8 +5,9 @@ module.exports = exports = function(app, apiRoutes, io){
 	app.get("/certificate/:id", function(req, res){
 		User.findOne({ _id : mongoose.Types.ObjectId(req.params.id)}).exec(function(err, data){
 			if(!err){
+				console.log("data", data);
  				var _html = _compiler.render({ _data : { name : "andrew alexander castro vital", cc : "1103102286"} }, 'certificate/certificate.ejs');
-
+				console.log("HTML", _html);
 				var wkhtmltopdf = require('wkhtmltopdf');
 				wkhtmltopdf.command = "/home/ec2-user/wkhtmltox/bin/wkhtmltopdf";
 
