dde3c2194c3239724bcf4c08c847458d3230fdbe
Armitage <listerine1989@gmail.com>
2018-03-07 07:46:03 -0500

    latest

--- controllers/User.js
+++ controllers/User.js
@@ -248,7 +248,6 @@ module.exports = function(app, apiRoutes){
          !REQ.email || (data.email = REQ.email);
          !REQ.name || (data.name = REQ.name);
          !REQ.last_name || (data.last_name = REQ.last_name);
-         !REQ.type || (data.type = REQ.type);
 
           console.log("data", data);
 
@@ -331,7 +330,7 @@ module.exports = function(app, apiRoutes){
     }
 
     function employees(req, res){
-        UserSchema.find({ "type" : $or : [{ type : "ADMINISTRATOR" }, { type : "SUPERVISOR" }] }).exec(function(err, users){
+        UserSchema.find({ "type" : $or : [ { type : "ADMINISTRATOR"}, {type : "SUPERVISOR"} ]}).exec(function(err, users){
             if(!err){
                 res.status(200).json(users);
             }else{
