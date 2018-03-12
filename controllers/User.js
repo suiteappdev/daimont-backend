9c3a23e9ab835e4f5bd7e5bf4d9c5e786b1c3937
Armitage <listerine1989@gmail.com>
2018-03-12 14:48:49 -0500

    latest

--- controllers/User.js
+++ controllers/User.js
@@ -550,6 +550,18 @@ module.exports = function(app, apiRoutes){
   }
 
 
+  function unblockCredit(req, res){
+      var REQ = req.body || req.params;
+
+      User.update({_id : mongoose.Types.ObjectId(req.params.id)}, { $unset: { "data.banned_time": 1 }},  function(err, user) {
+        if(!err){
+               res.status(200).json(user);
+          }
+      });
+  }
+
+
+
   function new_device(req, res){
          var data = {};
          var REQ = req.body || req.params;
@@ -603,6 +615,7 @@ module.exports = function(app, apiRoutes){
     apiRoutes.put("/user/updated/:id", updatedProfile);
     apiRoutes.put("/user/block/:id", block);
     apiRoutes.put("/user/unblock/:id", unblock);
+    apiRoutes.put("/user/unblockcredit/:id", unblockCredit);
     apiRoutes.put("/user/:id/update-cupon", update_cupon);
     apiRoutes.put("/user/:id/allow_cupon", allow_cupon);
     apiRoutes.delete("/user/:id", remove);
