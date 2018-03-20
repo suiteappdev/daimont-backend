35efa5f8561fb776c4737c1c338f7ea8dcc6ef19
Armitage <listerine1989@gmail.com>
2018-03-20 17:12:30 -0500

    latest

--- controllers/credits.js
+++ controllers/credits.js
@@ -904,7 +904,7 @@ module.exports = function(app, apiRoutes, io){
 				
 				Model.find({"data.hidden" : false, "data.status" : 'Consignado', "data.deposited_time_server" : { $gte: cutoffDate}}).sort("-createdAt").populate("_user").populate("_payment").populate("_contract").populate("_approvedby").exec(function(err, rs){
 					if(!err){
-						res.status(200).json(_updated_date || []);
+						res.status(200).json(rs || []);
 					}else{
 						res.status(500).json(err);
 					}
