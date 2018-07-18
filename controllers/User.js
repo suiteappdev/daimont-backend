f5fdd5cf1ee3f2206462b77b7f1ef267ab79addd
Armitage <listerine1989@gmail.com>
2018-07-18 12:16:56 -0500

    latest

--- controllers/User.js
+++ controllers/User.js
@@ -242,20 +242,8 @@ module.exports = function(app, apiRoutes){
     function update(req, res){
          var data = {};
          var REQ = req.body || req.params;
-         !REQ.metadata || (data.metadata = REQ.metadata);
-         !REQ.data || (data.data = REQ.data);
-         !REQ.username || (data.username = REQ.username);
-         !REQ.cc || (data.cc = REQ.cc);
-         !REQ.email || (data.email = REQ.email);
-         !REQ.name || (data.name = REQ.name);
-         !REQ.last_name || (data.last_name = REQ.last_name);
-         !REQ.type || (data.type = REQ.type);
 
-          console.log("data", data);
-
-          data = { $set : data }; 
-
-          user_manager.update({ _id : mongoose.Types.ObjectId(req.params.id) }, data, function(err, rs){
+          user_manager.update({ _id : mongoose.Types.ObjectId(req.params.id) }, REQ, function(err, rs){
               if(rs){
                   res.json(rs);
               }
