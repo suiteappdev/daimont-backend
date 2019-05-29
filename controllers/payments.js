686d11360537ec194ab95d59d0a7669958a1d394
Fastcode Dev <armitage@andrews-MacBook-Air.local>
2019-05-29 13:16:41 -0500

    latest

--- controllers/payments.js
+++ controllers/payments.js
@@ -238,20 +238,10 @@ module.exports = function(app, apiRoutes, io){
 
 		function update(req, res){
 			var data = {};
-			var REQ = req.body || req.params;
+			var REQ = req.body;
 			var where = {};
 
-			if(req.headers['x-daimont-user']){
-				where = { "metadata._author" : req.headers['x-daimont-user'] };
-			}
-
-			where._id = mongoose.Types.ObjectId(req.params.id);
-
-			!REQ.data || (data.data = REQ.data); 
-
-			data = { $set : data };          
-
-			Model.update( where , data , function(err, rs){
+			Model.update( { _id : mongoose.Types.ObjectId(req.params.id)} , REQ , function(err, rs){
 				if(rs){
 					res.status(200).json(rs);
 				}else{
